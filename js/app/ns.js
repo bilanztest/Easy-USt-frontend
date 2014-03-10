09b059e3aa0b5a18cf0727ccfe3256764af5ae02
Philipp Kyeck <philipp@beta-interactive.de>
2014-03-10 22:37:26 +0100

    add event pipeline to EAU namespace

--- js/app/ns.js
+++ js/app/ns.js
@@ -2,10 +2,16 @@ define(function (require) {
 
   "use strict";
 
+  var _ = require("underscore");
+  var Backbone = require("backbone");
+
   window.EAU = window.EAU || {};
   window.EAU.config = {
     
   };
+
+  // global event pipeline
+  window.EAU.vent = _.extend({}, Backbone.Events);
 
   // feature detection flags
   window.EAU.features = require("app/utils/features");
