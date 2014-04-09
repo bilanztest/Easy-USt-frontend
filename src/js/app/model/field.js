3868b73d6a5e6f077275cba74dd3d08f3e07541c
Aron Woost <aron@springworks.de>
2014-04-09 10:24:49 +0200

    Add ability to edit field

--- src/js/app/model/field.js
+++ src/js/app/model/field.js
@@ -20,15 +20,15 @@ define(function(require) {
     },
     
     parse: function(response) {
-      if (typeof response.value !== "undefined") {
+      if (response && typeof response.value !== "undefined") {
         response.value = parseFloat(response.value);
       }
 
-      if (typeof response.ust !== "undefined") {
+      if (response && typeof response.ust !== "undefined") {
         response.ust = parseInt(response.ust, 10);
       }
       
-      if (typeof response.date !== "undefined") {
+      if (response && typeof response.date !== "undefined") {
         response.date = new Date(response.date);
       }
 
@@ -37,23 +37,23 @@ define(function(require) {
 
     validate: function(attributes) {
       if (!validator.isLength(attributes.description, 1, 100)) {
-        return "description required but can't be longer than 100 characters";
+        return {fieldRef: "desc", msg: "description required but can't be longer than 100 characters"};
       }
 
       if (!validator.isFloat(attributes.value)) {
-        return "value has to be a number";
+        return {fieldRef:"val", msg: "value has to be a number"};
       }
 
       if (attributes.value <= 0 || attributes.value > 1000000) {
-        return "value has to be between 0 and 1000000";
+        return {fieldRef:"val", msg: "value has to be between 0 and 1000000"};
       }
       
       if (!validator.isIn(attributes.ust, [0, 7, 19])) {
-        return "ust has to be either 0, 7 or 19";
+        return {fieldRef:"ust", msg: "ust has to be either 0, 7 or 19"};
       }
 
       if (!validator.isIn(attributes.type, ["in", "out"])) {
-        return "type has to be either 'in' or 'out'";
+        return {fieldRef:"type", msg: "type has to be either 'in' or 'out'"};
       }
     }
 
--- src/js/app/view/field_view.jsx
+++ src/js/app/view/field_view.jsx
@@ -2,8 +2,11 @@ define(function(require) {
 
   "use strict";
 
+  var EAU = require("app/ns");
   var React = require("react");
   var formater = require("app/utils/formater");
+
+  var LayerAdd = require("jsx!app/view/layer_add");
 
   /**
    *
@@ -20,9 +23,20 @@ define(function(require) {
           <td>{this.props.field.get("value") + "€"}</td>
           <td>{this.props.field.get("ust") + "%"}</td>
           <td>{formater.round(this.props.field.get("value") * (this.props.field.get("ust")/100), 100) + "€"}</td>
+          <td><a href="#" onClick={this.onEditItem}>edit</a></td>
           <td><a href="#" onClick={this.onRemoveItem}>remove</a></td>
         </tr>
       )
+    },
+
+    onEditItem: function(event) {
+      event.preventDefault();
+
+      var comp = LayerAdd({
+        field: this.props.field
+      });
+
+      EAU.vent.trigger("openModal", comp);
     },
 
     onRemoveItem: function(event) {
